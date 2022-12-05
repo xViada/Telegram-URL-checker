4e9e47d6b314ba66f6a42a4a7516d1a0b300282f
xViada <aviadam09@gmail.com>
2022-12-05 23:58:05 +0100

    Snoopers hunter function added

--- run.py
+++ run.py
@@ -13,14 +13,31 @@ with open('config.txt') as c:
             a = line.strip('admin-user-ids:').split(', ')
             admin_ids = [eval(i) for i in a]
 
-
 bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
 def send(message):
     if message.from_user.id in admin_ids:
-        bot.reply_to(message, 'eyo')
+        bot.reply_to(message, 'Comandos disponibles: /help, /url_list, /add_url')
+    else:
+        s = f'username:{message.from_user.username}, id:{message.from_user.id}, full_name:' \
+            f'{message.from_user.full_name}, is_bot:{message.from_user.is_bot}\n'
+        print(('\nUNAUTHORIZED /START ATTEMPT\n' + s))
+        for id_user in admin_ids:
+            try:
+                bot.send_message(id_user, ('⚠️UNAUTHORIZED /START ATTEMPT⚠️\n' + s))
+            except:
+                pass
+        with open('snoopers.txt', 'a') as db:
+            db.write(s)
+
+
+@bot.message_handler(commands=['help'])
+def send(message):
+    if message.from_user.id in admin_ids:
+        bot.reply_to(message, '/url_list: Devuelve la lista de URLs que se estan checkeando actualmente.'
+                              '/add_url: Escribe: /add_url http://www.la-url.com para añadir una URL a la lista.')
 
 
 @bot.message_handler(commands=['add_url'])
@@ -37,11 +54,18 @@ def url_list(message):
             bot.reply_to(message, urls.read())
 
 
+# @bot.message_handler(commands=['snoopers'])
+
+
 def loop():
     while True:
         m = final_message()
         for id_user in admin_ids:
-            bot.send_message(id_user, m)
+            try:
+                bot.send_message(id_user, m)
+            except:
+                pass
+        print('Loop message sent.')
         sleep(1000)
 
 
