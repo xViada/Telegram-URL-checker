c5c5f69689e90bd61992f0ef636b66eb7e1cb0fb
xViada <aviadam09@gmail.com>
2022-12-06 01:30:26 +0100

    Added constants file and modded snooper.

---
+++ constants.py
@@ -0,0 +1,10 @@
+# List position
+COUNT_POS = 0
+ERROR_COUNT_POS = 1
+ERRORS_POS = 2
+
+# XPATHs
+MEGA_XPATH = '/html/body/div[6]/div[2]/div[2]/section/div[1]/div/div[4]/div[3]/div[2]/div[1]/button'
+PANEL_XPATH = '/html/body/div[1]/div/div[2]/form/button'
+TIKTOK_XPATH = '//*[@id="app"]/div[2]/div[2]/div/div[1]/div[1]/div[2]/div/div/button'
+RAW_XPATH = '/html/body/pre'
